31f626a7adaaeaf1e0a12237aa46289f80af3b9c
Ramon Cedeno <rcedeno@uinenlinea.mx>
2024-09-12 05:43:04 -0600

    Creating migrations, models and resources

---
+++ app/Http/Controllers/EmployeeTableController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\employee_table;
+use Illuminate\Http\Request;
+
+class EmployeeTableController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(employee_table $employee_table)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(employee_table $employee_table)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, employee_table $employee_table)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(employee_table $employee_table)
+    {
+        //
+    }
+}
---
+++ app/Models/pending_payments_table.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pending_payments_table extends Model
+{
+    use HasFactory;
+}
