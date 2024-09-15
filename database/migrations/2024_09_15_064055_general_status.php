6f31eb0c129b9a8cfb629d671b59e5e13fdaf00c
Ramon Cedeno <rcedeno@uinenlinea.mx>
2024-09-15 08:45:36 -0600

    table product created

---
+++ database/migrations/2024_09_15_064055_general_status.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('general_status', function (Blueprint $table) {
+            $table->id()->unsigned()->comment('Primary key, auto-incremental');
+            $table->string('general_status_name', 255)->comment('Name of the general state');
+            $table->boolean('status_is_account')->comment('Indicates true or false in the table of the same name');
+            $table->boolean('status_is_contract')->comment('Indicates true or false in the table of the same name');
+            $table->boolean('status_is_pay')->comment('Indicates true or false in the table of the same name');
+            $table->boolean('status_is_sale')->comment('Indicates true or false in the table of the same name');
+            $table->boolean('status_is_cancellation')->comment('Indicates true or false in the table of the same name');
+            $table->text('description')->comment('status description');
+            $table->timestamps();// Add created_at and updated_at
+            $table->softDeletes(); // Add deleted_at for logical deletes
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('general_status');
+    }
+
+};
---
+++ database/migrations/2024_09_15_074202_product.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id()->unsigned()->comment('Primary key, auto-incremental');
+            $table->string('product_id',10)->comment('id assigned to the product by the sponsor');
+            $table->string('product_name',255)->comment('code name assigned to the product');
+            $table->decimal('pricing', 5, 2)->comment('product price');
+            $table->timestamps();// Add created_at and updated_at
+            $table->softDeletes();// Add created_at and updated_at
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product');
+    }
+};
